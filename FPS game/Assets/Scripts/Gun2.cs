ef26d954745b66f949d889532f154ad53a5a9336
Martijn111 <martijncb@hotmail.com>
2018-08-14 14:04:48 +0200

    Add to FPS game

---
+++ FPS game/Assets/Scripts/Gun2.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Gun2 : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    public ParticleSystem Flash02;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    private AudioSource mAudioSrc;
+
+    private void Start()
+    {
+        mAudioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire2") && Time.time >= nextTimeToFire)
+        {
+            mAudioSrc.Play();
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Flash02.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
